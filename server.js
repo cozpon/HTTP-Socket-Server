f84e9a6db266cb3ecef03403e0cec43a92138787
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-10-02 18:57:38 -1000

    header data ready

--- server.js
+++ server.js
@@ -4,24 +4,45 @@ let PORT = process.env.PORT || 8080;
 const net = require('net'); // load TCP library
 const fs = require('fs');
 
+class Header{
+  constructor(method, url, http, accept, host, connection, contentType, contentLength){
+    this.method = method,
+    this.url = url,
+    this.http = http,
+    this.host = host,
+    this.connection = connection,
+    this.contentType = contentType,
+    this.contentLength = contentLength;
+  }
+}
+
 
 const server = net.createServer((request) => {
   request.on('data', (data) => {
+
     let dataInput = data.toString();
     let splitData = dataInput.split(`\n`);
-
-
     let methodRequest = splitData[0];
     let methodGetter = splitData[0].split(' ');
-    let method = methodGetter[0].split(' ' ); // returns GET or POST or SEND.. the METHOD
-    let ULI = methodGetter[1].split(' ' ); // returns the ULI
+    let method = methodGetter[0].split(' '); // returns GET or POST or SEND.. the METHOD
+    let ULI = methodGetter[1]; // returns the ULI
     let http = methodGetter[2].split(' '); // returns HTML + version
-    console.log(splitData);
+    let host = splitData[5].split(' ');
+    let connection = splitData[7].split(' ');
+    console.log(ULI);
 
-      request.write('data', (data) => {
-        console.log(method + " " + ULI + " " + http);
-      });
-    });
+    if (ULI === '/helium.html' || ULI === '/Helium.html'){
+      let ULI = '/helium.html';
+      let headerObj = new Header(method, ULI, http, host, connection);
+      console.log(headerObj, "EHHHEHHHEHHH");
+    }
+
+  // fs.readFile('/helium.html', (err, data) => {
+  //       if (err) throw err;
+  //       console.log(data);
+  //       });
+
+  });
 
   request.on('end', () => {
     console.log('Client disconnected');
