0484af2c2fd0a0cacc876c6febf3fcf819c252cb
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-10-03 11:55:19 -1000

    Server finally doing his thing correct

--- server.js
+++ server.js
@@ -4,61 +4,74 @@ let PORT = process.env.PORT || 8080;
 const net = require('net'); // load TCP library
 const fs = require('fs');
 
-class Header{
-  constructor(method, url, http, accept, host, connection, contentType, contentLength){
-    this.method = method,
-    this.url = url,
-    this.http = http,
-    this.host = host,
-    this.connection = connection,
-    this.contentType = contentType,
-    this.contentLength = contentLength;
-  }
-}
+
+    // HTTP 200 OK
+    // SERVER
+    // DATE
+    // CONTENT-TYPE
+    // CONTENT-LENGTH
+    // CONNECTION
+    // empty space
+
+    // HTML
+
+const header = function(file, method, ok, date, contentType, connection){
+  fs.readFile(file, (err, data) => {
+    let dataString = data.toString();
+    process.stdout.write(`\n${method}${ok}\n${date}\nContent-Type: ${contentType}\nContent-Length: ${dataString.length}\nConnection: ${connection}\n\n${dataString}`);
+  });
+};
 
 
 const server = net.createServer((request) => {
   request.on('data', (data) => {
+    let dataRequest = data.toString();
 
-    let dataInput = data.toString();
-    let splitData = dataInput.split(`\n`);
-    let methodRequest = splitData[0];
-    let methodGetter = splitData[0].split(' ');
-    let method = methodGetter[0].split(' '); // returns GET or POST or SEND.. the METHOD
-    let ULI = methodGetter[1]; // returns the ULI
-    let http = methodGetter[2].split(' '); // returns HTML + version
-    let host = splitData[5].split(' ');
-    let connection = splitData[7].split(' ');
-    console.log(ULI);
+    let splitData = dataRequest.split(`\n`);
+    let httpArray = splitData[0].split(' ');
 
-    if (ULI === '/helium.html' || ULI === '/Helium.html'){
-      let ULI = '/helium.html';
-      let headerObj = new Header(method, ULI, http, host, connection);
-      console.log(headerObj, "EHHHEHHHEHHH");
-    }
+    let requestArray = httpArray[0].split(' ');
 
-  // fs.readFile('/helium.html', (err, data) => {
-  //       if (err) throw err;
-  //       console.log(data);
-  //       });
+    // console.log("REQUEST", httpArray[1]);
+    let method = httpArray[0]; // returns GET or POST or SEND.. the METHOD
+    let path = httpArray[1]; // returns the ULI
+    let spec = httpArray[2]; // returns HTML + version
+    let ok = ' 200 OK'; // hard coded OK
+    let errorNotFound = ' 404 Not Found'; // hard coded error
+    let date = `Date: ${new Date().toUTCString()}`; // gets date of request
+    let htmlFile = 'text/html; charset=utf-8'; // hard code HTML file
+    let connType = 'keep-alive';  // hard code connection type
 
+
+    if (method === 'HEAD' || method === 'GET'){
+      console.log(path, "PATH IS HERE");
+      switch (path) {
+        case '/':
+          header('index.html', method, ok, date, htmlFile, connType);
+          break;
+        case '/index.html':
+          header('index.html', method, ok, date, htmlFile, connType);
+          break;
+        case '/helium.html':
+          header('helium.html', method, ok, date, htmlFile, connType);
+          break;
+        case '/hydrogen.html':
+          header('hydrogen.html', method, ok, date, htmlFile, connType);
+          break;
+        case 'styles.css':
+          header('styles.css', method, ok, date, htmlFile, connType);
+          break;
+        default:
+          header('404.html', method, errorNotFound, date, htmlFile, connType);
+      }
+    } else header('404.html', method, errorNotFound, date, htmlFile, connType);
+
+  request.end();
   });
-
-  request.on('end', () => {
-    console.log('Client disconnected');
-  });
-
 });
 
-
-
-server.on('error', (err) => {
-    console.log("Error in http domain:" + err);
-    throw err;
-  });
 server.listen(PORT, () => {
   console.log(`ServerBound, ${PORT}`, "daddio!");
   });
-
 
 
