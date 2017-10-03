8905b76903429d42a709fd8be07fcab57779084e
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-10-02 17:01:02 -1000

    push before dinner

--- server.js
+++ server.js
@@ -3,17 +3,28 @@ let PORT = process.env.PORT || 8080;
 //whatever is in the environment variable PORT, or 8080;
 const net = require('net'); // load TCP library
 const fs = require('fs');
-const server = net.createServer((request) => {
 
+
+const server = net.createServer((request) => {
   request.on('data', (data) => {
     let dataInput = data.toString();
     let splitData = dataInput.split(`\n`);
+
+
+    let methodRequest = splitData[0];
+    let methodGetter = splitData[0].split(' ');
+    let method = methodGetter[0].split(' ' ); // returns GET or POST or SEND.. the METHOD
+    let ULI = methodGetter[1].split(' ' ); // returns the ULI
+    let http = methodGetter[2].split(' '); // returns HTML + version
     console.log(splitData);
+
+      request.write('data', (data) => {
+        console.log(method + " " + ULI + " " + http);
+      });
     });
 
   request.on('end', () => {
     console.log('Client disconnected');
-
   });
 
 });
