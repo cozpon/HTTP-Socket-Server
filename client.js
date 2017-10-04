387ec13c3e88d5bf79a98e965f5f530c88572926
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-10-04 10:25:30 -1000

    Client getting Host/URI

--- client.js
+++ client.js
@@ -1,18 +1,35 @@
 // jshint esversion:6
 let PORT = process.env.PORT || 8080;
-console.log("yo daddio");
 const net = require('net');
 
-
-
-const options = {
-  port: 8080,
-  host: 'localhost'
-};
-const clientConnection = new net.connect(options, function(){
+const clientConnection = new net.connect(PORT, function(){
   console.log(`connected to server, ${PORT}` );
-  let allArgs = process.argv[2];
-  console.log(process.argv[2], "HEYEYE");
+  let notifier = process.argv[2];
+
+  if(notifier === undefined){
+    console.log("Yo daddio, you forgot to put in an argument.\nTry localhost:8080/index.html or something!");
+  }
+  else {
+    let directLink = notifier.split('/');
+    let host = directLink[0];
+    let uri = directLink[1];
+    let date = `Date: ${new Date().toUTCString()}`;
+
+    if(notifier.includes('-')){
+      console.log("yo u want the header?");
+      // I WANT HEADER
+    } else if(notifier.includes('/')){
+      // I'm a url and want a body
+      console.log(`take me to, Host: ${host}/${uri}`);
+    }
+    else{
+      console.log(`YO take me to, ${host}`);
+    }
+
+  }
+
+
+
 
   clientConnection.on('data', function(data){
     console.log(" HEY ");
@@ -31,3 +48,8 @@ const clientConnection = new net.connect(options, function(){
 // Connection: Keep-Alive
 // Accept: text/html, application/json
 // Content-Length: 278
+
+
+
+// if first character is '-' that's a flag that it's -I and wants header
+// otherwise it wants a URL and should listen for that.
